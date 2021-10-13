08aac1d4aee1017be0a98057e8406ffc8f6843fa
EC2 Default User <ec2-user@ip-172-31-69-104.ec2.internal>
2021-10-13 14:53:59 +0000

    [add]hedderlink

--- db/migrate/20211013091637_devise_create_users.rb
+++ db/migrate/20211013091637_devise_create_users.rb
@@ -34,7 +34,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       # 追加カラム
       t.string :name
-      
+      t.text :introduction
+      t.string :profile_image_id
+
       t.timestamps null: false
     end
 

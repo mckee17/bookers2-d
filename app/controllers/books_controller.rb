cbab7c8d8fd9bda5708e168271aa27466980c05b
EC2 Default User <ec2-user@ip-172-31-69-104.ec2.internal>
2021-10-15 10:47:34 +0000

    [add]link

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,8 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user.id
     @book.save
-    redirect_to root_path
+    redirect_to book_path(@book.id)
   end
 
   def edit
@@ -24,13 +23,13 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to root_path
+    redirect_to book_path(@book.id)
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to root_path
+    redirect_to books_path(@book.id)
   end
 
    private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,13 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
 
   def update
     @user = User.find(params[:id])
@@ -24,4 +31,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end

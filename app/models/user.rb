6302e7fa1d905fb23ce374521a6cf37cc68b5b41
EC2 Default User <ec2-user@ip-172-31-69-104.ec2.internal>
2021-10-20 14:36:38 +0000

    [add]commigrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
          
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   
   attachment :profile_image
   

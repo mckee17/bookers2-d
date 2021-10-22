331888d2237b763d40071345e37e6627a4230514
mckee17 <hayate0173@gamil.com>
2021-10-22 19:24:19 +0000

    [add]relationcreate

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,15 @@
 class RelationshipsController < ApplicationController
   def create
+    user = User.find(params[:user_id])
+    relation = Relationship.new
+    relation.follower_id = current_user.id
+    relation.followed_id = user.id
+    relation.save
+    redirect_back(fallback_location: users_path)
   end
-  
+
   def destroy
   end
+
 end
+

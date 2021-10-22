3aceedce4a5a8f772c2c619e88e35522551ac91d
mckee17 <hayate0173@gamil.com>
2021-10-22 16:46:59 +0000

    [add]controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about',as: 'about'
+  
   resources :books, only:[:index,:show,:create,:edit,:update,:destroy] do
     resource :favorites, only:[:create,:destroy]
     resources :book_comments,only:[:create,:destroy]
   end
-  resources :users, only:[:index,:show,:edit,:update]
+  
+  resources :users, only:[:index,:show,:edit,:update] do
+    resources :relationships,only:[:create,:destroy]
+  end
 end

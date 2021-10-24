c9187993e59168f00e326ece2c0c58ec13a98842
mckee17 <hayate0173@gamil.com>
2021-10-24 16:23:04 +0000

    [finish]

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,29 @@
 class SearchesController < ApplicationController
   def search
-    if params[:search_select] == 'user'
-      @users_search = User.where('name LIKE ?',"%#{params[:search]}%")
+    if params[:search_model] == 'user'
 
-    elsif params[:search_select] == 'book'
-      @books_search = Book.where('title LIKE ?',"%#{params[:search]}%")
+      if params[:search_match] == 'exact'
+        @users_search = User.where('name LIKE ?', params[:search])
+      elsif params[:search_match] == 'forward'
+        @users_search = User.where('name LIKE ?',"#{params[:search]}%")
+      elsif params[:search_match] == 'backward'
+        @users_search = User.where('name LIKE ?',"%#{params[:search]}")
+      elsif params[:search_match] == 'partial'
+        @users_search = User.where('name LIKE ?',"%#{params[:search]}%")
+      end
+
+    elsif params[:search_model] == 'book'
+
+      if params[:search_match] == 'exact'
+        @books_search = Book.where('title LIKE ?', params[:search])
+      elsif params[:search_match] == 'forward'
+        @books_search = Book.where('title LIKE ?',"#{params[:search]}%")
+      elsif params[:search_match] == 'backward'
+        @books_search = Book.where('title LIKE ?',"%#{params[:search]}")
+      elsif params[:search_match] == 'partial'
+        @books_search = Book.where('title LIKE ?',"%#{params[:search]}%")
+      end
+
     end
   end
 end
